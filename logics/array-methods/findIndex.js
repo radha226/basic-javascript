6355d159179a935a293d7f734c12b60b37c46a85
radha226 <radhasingh226@gmail.com>
2021-11-24 23:38:25 +0530

    start: findeIndex function

---
+++ logics/array-methods/findIndex.js
@@ -0,0 +1,15 @@
+
+const arr= [
+    {key1: "google-chrome", key2: 10},
+    {key1: "mozilla", key2: 5},
+    {key1: "IE", key2: 2},  
+    {key1: "google-chrome", key2: 10},
+    {key1: "google-chrome", key2: 10}, 
+    {key1: "IE", key2: 2},
+ ];
+//  Get the index of the object inside an array, matching a condition
+// Find index of object array
+// Find index of javascript "array of objects" based on object field value
+ arr.forEach(()=>{
+
+ })
